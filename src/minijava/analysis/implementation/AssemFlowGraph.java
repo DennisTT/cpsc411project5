989bd26ada83d95e1ecf17e035d0a061cc8e1407
Jeff Cheung <jeff.h.cheung@gmail.com>
2011-03-31 14:30:27 -0700

    Implemented def(), use(), and isMove() in AssemFlowGraph

--- src/minijava/analysis/implementation/AssemFlowGraph.java
+++ src/minijava/analysis/implementation/AssemFlowGraph.java
@@ -1,38 +1,59 @@
 package minijava.analysis.implementation;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import minijava.analysis.FlowGraph;
 import minijava.analysis.InterferenceGraph;
 import minijava.analysis.util.graph.Node;
+import minijava.codegen.assem.A_MOVE;
 import minijava.codegen.assem.Instr;
 import minijava.ir.temp.Temp;
 import minijava.util.List;
 
 public class AssemFlowGraph extends FlowGraph<Instr>
 {
+  private HashMap<Node<Instr>, Instr> table = new HashMap<Node<Instr>, Instr>();
+  
   public AssemFlowGraph(List<Instr> body)
   {
-    // TODO
+    Iterator<Instr> itr = body.iterator();
+    while(itr.hasNext())
+    {
+      Instr i = itr.next();
+      this.table.put(new Node<Instr>(this, i), i);
+    }
   }
 
   @Override
   public List<Temp> def(Node<Instr> node)
   {
-    // TODO Auto-generated method stub
-    return null;
+    Instr i = this.table.get(node);
+    if(i == null)
+    {
+      return null;
+    }
+    
+    return i.def();
   }
 
   @Override
   public List<Temp> use(Node<Instr> node)
   {
-    // TODO Auto-generated method stub
-    return null;
+    Instr i = this.table.get(node);
+    if(i == null)
+    {
+      return null;
+    }
+    
+    return i.use();
   }
 
   @Override
   public boolean isMove(Node<Instr> node)
   {
-    // TODO Auto-generated method stub
-    return false;
+    Instr i = this.table.get(node);
+    return (i != null) && (i instanceof A_MOVE);
   }
 
   @Override
